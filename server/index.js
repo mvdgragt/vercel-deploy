a5258185271c5b34c63d75709a8a675d945ac4f7
Michiel <47319476+mvdgragt@users.noreply.github.com>
2024-12-10 13:08:41 +0100

    changed cors to allow all

--- server/index.js
+++ server/index.js
@@ -7,11 +7,12 @@ import router from "./src/routes/itemRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(
-  cors({
-    origin: "https://vercel-deploy-client-pink.vercel.app", // Update this to your frontend URL
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://vercel-deploy-client-pink.vercel.app", // Update this to your frontend URL
+//   })
+// );
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
