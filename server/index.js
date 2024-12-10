4c97fb3f5bcf8622f67569ba5bb3dfe56dbea297
Michiel <47319476+mvdgragt@users.noreply.github.com>
2024-12-10 14:57:03 +0100

    editted vercel.json file

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-//import cors from "cors";
+import cors from "cors";
 import router from "./src/routes/itemRoutes.js";
 
 dotenv.config();
 
 const app = express();
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
